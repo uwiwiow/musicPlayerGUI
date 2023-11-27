4e57cd385192e206959b610b9841c05a73ce28d0
uwiwiow <uwiwiow@gmail.com>
2023-11-27 00:50:22 -0600

    fixed queue, time limit added

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import ast
 import tkinter
-
 import PIL.Image
 import pygame as pg
 import multiprocessing
@@ -16,16 +15,11 @@ window = ttk.Window(themename='darkly')
 window.title("Kilq Music Player")
 window.geometry('1600x900')
 window.iconbitmap('assets/icon.ico')
-# window.state('zoomed')  # TODO uncomment this line
+# window.state('zoomed')
 
 # pg
 pg.mixer.init()
-# TODO arreglar el inicio si no hayun pickle
 music = Music().list_songs()
-
-
-def reload_song_img():
-    print(1)
 
 
 def on_enter(event):
@@ -75,19 +69,24 @@ def play():
     pg.mixer_music.load(song[0])
     pg.mixer_music.play(1)  # TODO buscar algo que siempre se este actualizando que verifique si paso o esta reproduciendo la siguiente cancion
     # TODO o sino en lugar de hacer pg.mixer_music.queue(song_data[0]) , no hacer queue y cada que verifique que se termino una cancion volver a llamar play()
+    # TODO si empezo la siguiente cancion de la lista quitar su tiempo de el maximo y quitar el error, probablemente se quite solo con el update_queue()
     dll.pop()  # TODO al agregar a queue (funcion abajo) o hacer play hacer que se actualice el frame de queue
     songs_var.set(dll.display(wdata='dict'))
     update_queue()
 
 
 def add_to_queue(song_data: tuple[str, dict, PIL.Image.Image]):
-    dll.append(song_data)
-    songs_var.set(dll.display(wdata='dict'))
-    pg.mixer_music.queue(song_data[0])
-    update_queue()
-    # TODO anadir el tiempo total para no pasar los 15 minutos
-    if not pg.mixer_music.get_busy():
-        play()
+    if (time_var.get() + song_data[1]['duration']) <= 900:
+        time_var.set(time_var.get() + song_data[1]['duration'])
+        dll.append(song_data)
+        songs_var.set(dll.display(wdata='dict'))
+        pg.mixer_music.queue(song_data[0])
+        update_queue()
+        if not pg.mixer_music.get_busy():
+            time_var.set(time_var.get() - song_data[1]['duration'])
+            play()
+    else:
+        time_limit()
 
 
 # cant be pickled
@@ -99,8 +98,8 @@ queue_image = open_img('assets/queue.png')
 add = lambda x: '0' + str(x) if len(str(x)) == 1 else x
 
 # set app sizes for low res
-fhd = [60, 45, 25, 25]
-hd = [40, 30, 10, 15]
+fhd = [60, 45, 25, 25, 600]
+hd = [40, 30, 10, 15, 450]
 res: list[int] = hd if window.winfo_width() < 1880 else fhd
 
 # main frame with scroller
@@ -112,101 +111,101 @@ sf.pack(fill=BOTH, expand=YES)
 dll = DLL()
 songs_var = ttk.StringVar()
 songs_var.set(dll.display(wdata='dict'))
+time_var = ttk.IntVar()
 
-# quitar que sea def
-def display_body():
-    for i, v in enumerate(music):
-        frame = ttk.Frame(sf, name=f'song{i}')  # individual frame for each song
+for i, v in enumerate(music):
+    frame = ttk.Frame(sf, name=f'song{i}')  # individual frame for each song
 
-        number = ttk.Button(frame, text=f'{i + 1}', width=3, image=None, bootstyle='default-link', name='play_button',
-                            command=lambda song_data=v: load_song(song_data))
-        number.pack(side='left', padx=(20, 10))
+    number = ttk.Button(frame, text=f'{i + 1}', width=3, image=None, bootstyle='default-link', name='play_button',
+                        command=lambda song_data=v: load_song(song_data))
+    number.pack(side='left', padx=(20, 10))
 
-        try:
-            image_label = ttk.Label(frame, image=v[2])
-        except tkinter.TclError:
-            image_label = ttk.Label(frame, image=None, name='image')
-        image_label.pack(side='left', padx=10)
-        print(image_label)
+    image_label = ttk.Label(frame, image=v[2])
+    image_label.pack(side='left', padx=10)
 
-        data_frame = ttk.Frame(frame, width=res[0])
-        title = ttk.Label(data_frame, text=f'{v[1]["title"]}', width=res[0], font='14')
-        title.pack()
-        autor = ttk.Label(data_frame, text=f'{v[1]["artist"]}', width=res[0], font='12', bootstyle='light')
-        autor.pack()
-        data_frame.pack(side='left', padx=15)
+    data_frame = ttk.Frame(frame, width=res[0])
+    title = ttk.Label(data_frame, text=f'{v[1]["title"]}', width=res[0], font='14')
+    title.pack()
+    autor = ttk.Label(data_frame, text=f'{v[1]["artist"]}', width=res[0], font='12', bootstyle='light')
+    autor.pack()
+    data_frame.pack(side='left', padx=15)
 
-        album = ttk.Label(frame, text=f'{v[1]["album"]}', width=res[1], font='14')
-        album.pack(side='left', padx=15)
+    album = ttk.Label(frame, text=f'{v[1]["album"]}', width=res[1], font='14')
+    album.pack(side='left', padx=15)
 
-        fecha = ttk.Label(frame, text=f'{v[1]["year"]}', width=res[2], font='14')
-        fecha.pack(side='left', padx=15)
+    fecha = ttk.Label(frame, text=f'{v[1]["year"]}', width=res[2], font='14')
+    fecha.pack(side='left', padx=15)
 
-        genero = ttk.Label(frame, text=f'{v[1]["genre"]}', width=res[3], font='14')
-        genero.pack(side='left', padx=15)
+    genero = ttk.Label(frame, text=f'{v[1]["genre"]}', width=res[3], font='14')
+    genero.pack(side='left', padx=15)
 
-        duration = ttk.Label(frame, text=f'{add(int(v[1]["duration"]) // 60)}:{add(int(v[1]["duration"]) % 60)}',
-                             width=10,
-                             font='14')
-        duration.pack(side='left', padx=15)
+    duration = ttk.Label(frame, text=f'{add(int(v[1]["duration"]) // 60)}:{add(int(v[1]["duration"]) % 60)}',
+                         width=10,
+                         font='14')
+    duration.pack(side='left', padx=15)
 
-        queue = ttk.Button(frame, width=3, image=add_queue_image, bootstyle='default-link', name='queue_button',
-                           command=lambda song_data=v: add_to_queue(song_data))
-        queue.pack(side='left', padx=15)
+    queue = ttk.Button(frame, width=3, image=add_queue_image, bootstyle='default-link', name='queue_button',
+                       command=lambda song_data=v: add_to_queue(song_data))
+    queue.pack(side='left', padx=15)
 
-        frame.pack(ipady=10, anchor='w', fill='x')
-        frame.bind("<Enter>", on_enter)
-        frame.bind("<Leave>", on_leave)
+    frame.pack(ipady=10, anchor='w', fill='x')
+    frame.bind("<Enter>", on_enter)
+    frame.bind("<Leave>", on_leave)
 
 
 def mostrar_lista_canciones():
     if button_var.get():
-        for child in window.winfo_children():
-            if child.winfo_name() == 'float_frame':
-                button_var.set(ttk.FALSE)
-                child.destroy()
+        button_var.set(ttk.FALSE)
+        window.nametowidget('float_frame').destroy()
     else:
         button_var.set(ttk.TRUE)
         # Crea un nuevo Frame para mostrar la lista de canciones
-        frame_canciones = ttk.Frame(window, padding=1, width=420, height=600, borderwidth=1, relief='solid',
+        frame_canciones = ttk.Frame(window, padding=1, width=420, height=res[4], borderwidth=1, relief='solid',
                                     bootstyle='dark', name='float_frame')
         frame_canciones.propagate(0)
         frame_canciones.place(relx=1.0, rely=1.0, x=-20, y=-70, anchor="se")
 
-        ttk.Label(frame_canciones, text="Lista de Canciones", font='arial 16 bold', bootstyle='inverse-dark').pack()
+        ttk.Label(frame_canciones, text="Lista de Canciones", font='arial 16 bold', bootstyle='inverse-dark', name='label_lista_canciones').pack()
+        ttk.Label(frame_canciones, text=f'{add(time_var.get() // 60)}:{add(time_var.get() % 60)}', font='arial 16 bold', bootstyle='inverse-dark', name='tiempo').pack()
 
-        sf_canciones = ScrolledFrame(frame_canciones, name='scrolledFrame1')
-        # sf_canciones.hide_scrollbars()
+        sf_canciones = ScrolledFrame(frame_canciones, name='frame_scroll')
+        sf_canciones.hide_scrollbars()
         sf_canciones.pack(fill=BOTH, expand=YES)
 
         update_queue()
 
 
 def update_queue():
-    for child in window.winfo_children():  # root
-        if child.winfo_name() == 'float_frame':  # float
-            for widgets in child.winfo_children():
-                if isinstance(widgets, ttk.Frame):
-                    widgets.destroy()
+    if button_var.get():
+        sf_widget = window.nametowidget('.float_frame.!frame.frame_scroll')
+        window.nametowidget('.float_frame.tiempo')['text'] = f'{add(time_var.get() // 60)}:{add(time_var.get() % 60)}'
+        for child in sf_widget.winfo_children():
+            child.destroy()
 
-            sf_canciones = ScrolledFrame(child, name='scrolledFrame1')
-            # sf_canciones.hide_scrollbars()
-            sf_canciones.pack(fill=BOTH, expand=YES)
-
-            # TODO no llame si esta vacio songs_var
+        try:
             songs = [ast.literal_eval(song) for song in ast.literal_eval(songs_var.get())]
 
             for it, song_data in enumerate(songs):
-                song_frame = ttk.Frame(sf_canciones)
+                song_frame = ttk.Frame(sf_widget, name=f'song{it}')
                 number = ttk.Label(song_frame, text=f'{it + 1}', width=3, image=None, name=str(it), font='14')
                 number.pack(side='left', padx=(20, 10))
 
                 title = ttk.Label(song_frame, text=f'{song_data["title"]}', width=45, font='14')
                 title.pack(side='left', padx=15)
                 song_frame.pack(ipady=20, anchor='w', fill='x')
+        except SyntaxError:
+            ...
 
 
-display_body()
+def time_limit():
+    if button_var.get():
+        sf_widget = window.nametowidget('.float_frame.!frame.frame_scroll')
+
+        error_frame = ttk.Frame(sf_widget, name='error_frame')
+        title = ttk.Label(error_frame, text='Tiempo limite alcanzado: maximo 15 minutos', width=45, font='20', bootstyle='danger')
+        title.pack(side='left', padx=15)
+        error_frame.pack(ipady=20, anchor='w', fill='x')
+
 
 bottom_frame = ttk.Frame(window, bootstyle='dark', name='bottom_frame')
 bottom_frame.pack(side='bottom', fill='x')
